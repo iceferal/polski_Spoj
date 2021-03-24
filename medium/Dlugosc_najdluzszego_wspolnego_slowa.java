743de9e484f3b815677aa8e7de25b027cd38d108
mr.alien <iceferal@gmail.com>
2021-03-24 19:29:06 +0100

    ehh

---
+++ medium/Dlugosc_najdluzszego_wspolnego_slowa.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Dlugosc_najdluzszego_wspolnego_slowa {
+    
+    public static void slowko(String b, String d) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        for(int i=0; i<x; i++) {
+            int a = scanner.nextInt();
+            String b = scanner.next();
+            int c = scanner.nextInt();
+            String d = scanner.next();
+            slowko(b, d);
+        }
+    }
+}
--- medium/Permutacje.java
+++ medium/Permutacje.java
@@ -2,15 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Permutacje {
- 
-    
+
+    public static void permututu(int c) {
+        Set<String> strings = new TreeSet<String>();
+        if(c == 1)
+            System.out.println("a");
+        else {            
+            String buffer = "";
+            String fin = "";
+            for(int i=97; i<97+c; i++)
+                buffer = buffer + (char)i;
+            char[] bufi = buffer.toCharArray();
+
+            for(int x=0; x<bufi.length; x++) 
+                for(int y=0; y<bufi.length; y++) {
+                    char[] temp = bufi;
+                    char tmp = temp[x];
+                    temp[x] = temp[y];
+                    temp[y] = tmp;
+                strings.add(String.valueOf(temp));      }
+            System.out.println(strings);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         for(int i=0; i<x; i++) {
             int a = scanner.nextInt();
-            
+            permututu(a);
         }
     }
 }
