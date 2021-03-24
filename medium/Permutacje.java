14cb3f217bb36178e7764eab5d2644ad112101c5
mr.alien <iceferal@gmail.com>
2021-03-24 16:47:55 +0100

    up

---
+++ medium/Permutacje.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Permutacje {
+ 
+    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        for(int i=0; i<x; i++) {
+            int a = scanner.nextInt();
+            
+        }
+    }
+}
