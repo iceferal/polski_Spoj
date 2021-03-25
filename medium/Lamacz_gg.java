1e8a8f15050015f34f318b8c29cf705bdc6f81c9
mr.alien <iceferal@gmail.com>
2021-03-25 16:00:24 +0100

    gg

--- medium/Lamacz_gg.java
+++ medium/Lamacz_gg.java
@@ -1,3 +1,18 @@
+import java.util.*;
+import java.io.*;
+
 public class Lamacz_gg {
     
+    public static void gugu(String linja) {
+
+
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext(pattern)) {
+            String linja = scanner.hasNext();
+            gugu(linja);       }
+    }
 }
