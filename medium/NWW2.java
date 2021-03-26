7a5afd9e1ed2085bfc3af1f6cb3faace9ebb62f1
mr.alien <iceferal@gmail.com>
2021-03-26 17:02:44 +0100

    jas

--- medium/NWW2.java
+++ medium/NWW2.java
@@ -4,10 +4,23 @@ import java.math.BigInteger;
 
 public class NWW2 {
 
-    public static void enwuwu(BigInteger[] tab) {
+    public static long enwude(long a, long b) {
+        while(a != b) {
+            if(a > b)
+                a = a - b;
+            else if(a < b)
+                b = b - a;    }
+        return a;    }
 
+    public static long enwuwu(long a, long b) {
+        return (a*b) / enwude(a, b);    }    
 
+    public static void enwuwu(long[] tab) {
+        long result = 1;
+        for(int i=0; i<tab.length; i++)
+            result = enwuwu(result, tab[i]);
 
+        System.out.println(result);
     }
     
 
@@ -16,9 +29,9 @@ public class NWW2 {
         int a = scanner.nextInt();
         for(int i=0; i<a; i++) {
             int b = scanner.nextInt();
-            BigInteger[] tab = new BigInteger[b];
+            long[] tab = new long[b];
             for(int j=0; j<b; j++) {
-                tab[j] = scanner.nextBigInteger();
+                tab[j] = scanner.nextLong();
             }
             enwuwu(tab);
         }
