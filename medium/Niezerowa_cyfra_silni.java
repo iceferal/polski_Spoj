ea1334ad2aec94ce0f0379f292d2957cb0875159
mr.alien <iceferal@gmail.com>
2021-05-11 17:17:28 +0200

    actual

--- medium/Niezerowa_cyfra_silni.java
+++ medium/Niezerowa_cyfra_silni.java
@@ -16,7 +16,7 @@ public class Niezerowa_cyfra_silni {
         System.out.println(fin.charAt(fin.length()-1));
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
         int hw = scanner.nextInt();
         for(int i =0; i<hw; i++) {
